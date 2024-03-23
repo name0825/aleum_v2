f6a36f274161204e1a25846488c740ed953d5b26
name0825 <qkralswo0825@naver.com>
2024-03-24 07:05:44 +0900

    Change the error log from syslog to error_log

--- public/index.php
+++ public/index.php
@@ -3,9 +3,14 @@
     try {
         include __DIR__."/../module/controller.php";
         Aleum\Router\Router::exec_main($path);
-        if (error_get_last() !== NULL)
+        if (error_get_last() !== NULL) {
+            ob_clean();
+            error_log(error_get_last()["message"]);
             Aleum\Utils\Error\abort(500);
+        }
     } catch (Exception $e) {
+        ob_clean();
+        error_log($e->getMessage());
         Aleum\Utils\Error\abort(500);
     }
     ob_end_flush();
